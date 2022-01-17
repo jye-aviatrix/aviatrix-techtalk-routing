628290ea7720bc1098c0f94dd37f479f9f2080dd
Jun Ye <jye@aviatrix.com>
2022-01-17 18:32:03 -0500

    ue1 prod to transit

--- spoke-to-transit-attachment.tf
+++ spoke-to-transit-attachment.tf
@@ -19,3 +19,10 @@ resource "aviatrix_spoke_transit_attachment" "wu2_qa_to_transit" {
   spoke_gw_name   = aviatrix_spoke_gateway.az_wu2_spoke_qa_gw.gw_name
   transit_gw_name = aviatrix_transit_gateway.az_wu2_transit_firenet_gw.gw_name
 }
+
+
+# AWS us-east-1 prod to transit
+resource "aviatrix_spoke_transit_attachment" "ue1_prod_to_transit" {
+  spoke_gw_name   = aviatrix_spoke_gateway.aws_ue1_spoke_prod_gw.gw_name
+  transit_gw_name = aviatrix_transit_gateway.aws_ue1_transit_gw.gw_name
+}
