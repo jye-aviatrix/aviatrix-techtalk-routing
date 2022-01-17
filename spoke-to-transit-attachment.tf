fa9538849dd9dd35b4d6ff0f982d5adf888e2205
Jun Ye <jye@aviatrix.com>
2022-01-17 16:58:31 -0500

    change to use gw name

--- spoke-to-transit-attachment.tf
+++ spoke-to-transit-attachment.tf
@@ -1,5 +1,5 @@
 # Create an Aviatrix Spoke Transit Attachment
 resource "aviatrix_spoke_transit_attachment" "ue2_shared_service_to_transit" {
-  spoke_gw_name   = aviatrix_spoke_gateway.aws_ue2_spoke_shared_services_gw
-  transit_gw_name = aviatrix_transit_gateway.aws_ue2_transit_gw
+  spoke_gw_name   = aviatrix_spoke_gateway.aws_ue2_spoke_shared_services_gw.gw_name
+  transit_gw_name = aviatrix_transit_gateway.aws_ue2_transit_gw.gw_name
 }
