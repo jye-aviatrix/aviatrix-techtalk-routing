427e95958ced38717a0bc8052afa491a6847ce50
Jun Ye <jye@aviatrix.com>
2022-01-17 11:54:10 -0500

    add required providers

--- provider.tf
+++ provider.tf
@@ -1,3 +1,18 @@
+terraform {
+  required_providers {
+    aviatrix = {
+      source  = "AviatrixSystems/aviatrix"
+      version = "~> 2.20"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.91.0"
+    }
+  }
+  required_version = ">= 1.0"
+}
+
+
 # Configure Aviatrix provider
 provider "aviatrix" {
   controller_ip           = var.controller_ip
