b5f78ebdf185a3d7aac4fba3bb69b1a3e513affd
Jun Ye <jye@aviatrix.com>
2022-01-17 20:28:44 -0500

    add subnet

--- csr.tf
+++ csr.tf
@@ -4,3 +4,12 @@ resource "aws_vpc" "main" {
     Name = "main"
   }
 }
+
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.128.80.0/24"
+
+  tags = {
+    Name = "Main"
+  }
+}
