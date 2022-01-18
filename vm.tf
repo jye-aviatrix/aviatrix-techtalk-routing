32539343408a029d3781acdb1bb16e194e46af30
Jun Ye <jye@aviatrix.com>
2022-01-18 14:51:49 -0500

    update code

--- vm.tf
+++ vm.tf
@@ -1,8 +1,8 @@
 module "azwu2prod" {
   source = "./modules/azure-linux-vm"
   vm_name = "azwu2prod"
-  resource_group_name = aviatrix_vpc.az_wu2_spoke_prod_vpc.resource_group_name
-  location = aviatrix_vpc.az_wu2_spoke_prod_vpc.location
+  resource_group_name = aviatrix_vpc.az_wu2_spoke_prod_vpc.resource_group
+  location = aviatrix_vpc.az_wu2_spoke_prod_vpc.region
   subnet_id = aviatrix_vpc.az_wu2_spoke_prod_vpc.public_subnets[1].subnet_id
   public_key_data = var.public_key_data
   admin_username = var.admin_username
