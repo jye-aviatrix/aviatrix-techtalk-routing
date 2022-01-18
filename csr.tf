6c78fa670d3d0b5deac3d653d72e056997f09ea6
Jun Ye <jye@aviatrix.com>
2022-01-17 20:30:55 -0500

    add route table

--- csr.tf
+++ csr.tf
@@ -14,8 +14,21 @@ resource "aws_subnet" "main" {
   }
 }
 
-resource "aws_internet_gateway" "gw" {
+resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main"
+  }
+}
+
+resource "aws_route_table" "main" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
 
   tags = {
     Name = "main"
