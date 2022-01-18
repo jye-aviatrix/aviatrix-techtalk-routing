0ea8e856d0d1c8b41d085e0eec1a3d2d6f0eed9f
Jun Ye <jye@aviatrix.com>
2022-01-18 13:23:08 -0500

    add comment

--- spoke-to-transit-attachment.tf
+++ spoke-to-transit-attachment.tf
@@ -1,6 +1,6 @@
 # Create an Aviatrix Spoke Transit Attachment
 
-AWS us-east-2 qa to transit
+# AWS us-east-2 qa to transit
 resource "aviatrix_spoke_transit_attachment" "ue2_qa_to_transit" {
   spoke_gw_name   = aviatrix_spoke_gateway.aws_ue2_spoke_qa_gw.gw_name
   transit_gw_name = aviatrix_transit_gateway.aws_ue2_transit_gw.gw_name
