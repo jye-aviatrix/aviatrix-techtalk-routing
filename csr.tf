8c4f44beb832b95df38491d7474d3df67ebf433f
Jun Ye <jye@aviatrix.com>
2022-01-17 20:33:39 -0500

    add route association

--- csr.tf
+++ csr.tf
@@ -34,3 +34,9 @@ resource "aws_route_table" "main" {
     Name = "main"
   }
 }
+
+
+resource "aws_route_table_association" "main" {
+  subnet_id      = aws_subnet.main.id
+  route_table_id = aws_route_table.main.id
+}
